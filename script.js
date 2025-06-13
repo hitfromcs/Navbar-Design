4cdbdfccc7ec8a005c7a4759e9a201d7113b0b10
Arham ahmed <arhamahmed8699@gmail.com>
2025-06-13 18:32:43 +0500

    v1.0
    Includes a complete header with semantics.

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSidebar(){
+    const sidebar = document.querySelector('.nav_sidebar')
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar(){
+    const sidebar2 =document.querySelector('.nav_sidebar')
+    sidebar2.style.display = 'none';
+}
